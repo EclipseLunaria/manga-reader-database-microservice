a834af9a9f020c0555ef7e0fce8d17fd38c8ee95
Ecliptek-exe <dorsettj@gmail.com>
2024-07-15 08:34:31 -0700

    include metadata endpoint

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,12 @@ import 'reflect-metadata';
 import express from 'express';
 import { createConnection } from 'typeorm';
 import { ChapterInfo } from './models/mangaInfoModel';
+import mangaMetaRouter from './routes/mangaMetaRoute';
 
 createConnection().then(async connection => {
   const app = express();
   const port = process.env.PORT || 6965;
-  
+  app.use('/metadata', mangaMetaRouter);
   app.listen(port, () => {
     console.log(`Database service is running on http://localhost:${port}`);
   });
